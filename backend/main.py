1f3e10e5936d2d0a03bf5d779fdbe4216feecb94
kunal779 <kunalghugare28@gmail.com>
2025-11-26 18:49:35 +0530

    Fixed CORS for production

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-     allow_origins=["*"],
+    allow_origins=["*"],        # important
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
