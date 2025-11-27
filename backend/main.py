4c6bbee6c9316d2b65044a8b541359f3e5b83bc8
kunal779 <kunalghugare28@gmail.com>
2025-11-27 22:09:27 +0530

    Open CORS for all origins

--- backend/main.py
+++ backend/main.py
@@ -14,15 +14,15 @@ ALGORITHM = "HS256"
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-    "https://nagster.vercel.app",
-]
+# origins = [
+#     "http://127.0.0.1:5173",
+#     "http://localhost:5173",
+#     "https://nagster.vercel.app",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
